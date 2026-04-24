843f4323ff1413e04454829b935aa8ac3421102a
Ruslan Abdrashitov <31285265+ruslan-ab@users.noreply.github.com>
2026-04-24 12:45:13 -0700

    Add: builder - submission-0

---
+++ Design Patterns/builder/submission-0.py
@@ -0,0 +1,56 @@
+class Meal:
+    def __init__(self):
+        self.cost = 0.0
+        self.takeOut = False
+        self.main = ""
+        self.drink = ""
+
+    def getCost(self) -> float:
+        return self.cost
+
+    def setCost(self, cost: float) -> None:
+        self.cost = cost
+
+    def getTakeOut(self) -> bool:
+        return self.takeOut
+
+    def setTakeOut(self, takeOut: bool) -> None:
+        self.takeOut = takeOut
+
+    def getMain(self) -> str:
+        return self.main
+
+    def setMain(self, main: str) -> None:
+        self.main = main
+
+    def getDrink(self) -> str:
+        return self.drink
+
+    def setDrink(self, drink: str) -> None:
+        self.drink = drink
+
+
+class MealBuilder:
+    
+    def __init__(self):
+        self._meal = Meal()
+
+    def addCost(self, cost: float) -> 'MealBuilder':
+        self._meal.setCost(cost)
+        return self
+
+    def addTakeOut(self, takeOut: bool) -> 'MealBuilder':
+        self._meal.setTakeOut(takeOut)
+        return self
+
+    def addMainCourse(self, main: str) -> 'MealBuilder':
+        self._meal.setMain(main)
+        return self
+
+    def addDrink(self, drink: str) -> 'MealBuilder':
+        self._meal.setDrink(drink)
+        return self
+
+    def build(self) -> Meal:
+        return self._meal
+
