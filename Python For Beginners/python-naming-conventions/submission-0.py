f7a8d4fd20d53c9bcb9c373bcbe2adc16822538f
Ruslan Abdrashitov <31285265+ruslan-ab@users.noreply.github.com>
2026-06-26 19:08:58 -0700

    Delete Python For Beginners directory
    Didn't mean to commit this

--- Python For Beginners/python-naming-conventions/submission-0.py
+++
@@ -1,7 +0,0 @@
-python_creation_date = "February, 1991"
-java_creation_date = "May, 1995"
-javascript_creation_date = "December, 1995"
-
-print(python_creation_date)
-print(java_creation_date)
-print(javascript_creation_date)
